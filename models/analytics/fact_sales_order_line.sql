a4934bec3f6495782187b6093c62073ba8b29019
pphuongg97 <phuongpham19.198@gmail.com>
2023-03-04 13:45:42 +0700

    add gross amount

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -2,4 +2,5 @@ SELECT
   order_line_id as sales_order_line_key
   ,quantity as quantity
   ,unit_price as unit_price
+  ,quantity*unit_price as gross_amount
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
