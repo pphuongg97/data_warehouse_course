f598709cede8e49d964754489c81684a92f66bbb
pphuongg97 <phuongpham19.198@gmail.com>
2023-03-04 14:12:37 +0700

    add product key

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -3,4 +3,5 @@ SELECT
   ,quantity as quantity
   ,unit_price as unit_price
   ,quantity*unit_price as gross_amount
+  ,stock_item_id as product_key
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
