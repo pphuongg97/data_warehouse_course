45f7258fefbb7f0b08b427cdfecfd60fc65722eb
pphuongg97 <phuongpham19.198@gmail.com>
2023-03-09 17:31:13 +0700

    cte rename column

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,5 +1,12 @@
-SELECT 
-  cast(stock_item_id as int) as product_key
-  ,cast(stock_item_name as string) as product_name
-  ,cast(brand as string) as brand_name
-FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+WITH dim_product_source as(
+  SELECT *
+  FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+)
+
+, dim_product_rename_column as(
+  SELECT 
+  stock_item_id as product_key,
+  stock_item_name as product_name,
+  brand as brand_name
+  FROM dim_product_source
+)
