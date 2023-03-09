0c7209ff39079f4299532160c79358799ae40035
pphuongg97 <phuongpham19.198@gmail.com>
2023-03-09 19:17:56 +0700

    dim customer cast type

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -10,3 +10,14 @@ FROM `vit-lam-data.wide_world_importers.sales__customers`
   FROM dim_customer_source
 )
 
+,dim_customer_cast_type AS(
+  SELECT
+  CAST(customer_key AS INT) AS customer_key
+  ,CAST(customer_name AS STRING) AS customer_name
+  FROM dim_customer_rename_column
+)
+
+SELECT
+customer_key
+,customer_name
+FROM dim_customer_cast_type
