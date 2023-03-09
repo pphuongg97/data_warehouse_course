2a3663ca79dc59d2c4987bb9d6c883563a5f9631
pphuongg97 <phuongpham19.198@gmail.com>
2023-03-09 17:43:43 +0700

    fact sales order line rename column

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,7 +1,15 @@
+WITH fact_sales_order_line_source AS (
+  SELECT *
+  FROM `vit-lam-data.wide_world_importers.sales__order_lines`
+)
+
+, fact_sales_order_line_rename_column AS(
 SELECT 
-  cast(order_line_id as int) as sales_order_line_key
-  ,cast (quantity as int) as quantity
-  ,cast(unit_price as numeric) as unit_price
-  ,cast(quantity as int)* cast(unit_price as numeric) as gross_amount
-  ,cast(stock_item_id as int) as product_key
-FROM `vit-lam-data.wide_world_importers.sales__order_lines`
+  order_line_id as sales_order_line_key
+  ,stock_item_id as product_key
+  ,quantity as quantity
+  ,unit_price as unit_price  
+FROM fact_sales_order_line_source
+)
+
+
