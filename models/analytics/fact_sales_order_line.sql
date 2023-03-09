ebee41181565274691200e4f37d1c929e47bea71
pphuongg97 <phuongpham19.198@gmail.com>
2023-03-09 17:18:24 +0700

    change formula cast for gross amount

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -2,6 +2,6 @@ SELECT
   cast(order_line_id as int) as sales_order_line_key
   ,cast (quantity as int) as quantity
   ,cast(unit_price as numeric) as unit_price
-  ,cast((quantity*unit_price) as numeric) as gross_amount
+  ,cast(quantity as int)* cast(unit_price as numeric) as gross_amount
   ,cast(stock_item_id as int) as product_key
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
