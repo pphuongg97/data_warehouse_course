1052bd0f5c8218ffc4dbfd8eba0b0bb1a5ea79b0
pphuongg97 <phuongpham19.198@gmail.com>
2023-03-11 15:59:08 +0700

    add customer category key and buying group key

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -6,18 +6,24 @@ FROM `vit-lam-data.wide_world_importers.sales__customers`
 ,dim_customer_rename_column AS(
   SELECT
   customer_id AS customer_key
-  ,customer_name AS customer_name
+  , customer_name AS customer_name
+  , customer_category_id AS customer_category_key
+  , buying_group_id AS buying_group_key 
   FROM dim_customer_source
 )
 
 ,dim_customer_cast_type AS(
   SELECT
   CAST(customer_key AS INT) AS customer_key
-  ,CAST(customer_name AS STRING) AS customer_name
+  , CAST(customer_name AS STRING) AS customer_name
+  , CAST(customer_category_key AS INT) AS customer_category_key
+  , CAST (buying_group_key AS FLOAT) AS buying_group_key
   FROM dim_customer_rename_column
 )
 
 SELECT
 customer_key
-,customer_name
+, customer_name
+, customer_category_key
+, buying_group_key
 FROM dim_customer_cast_type
