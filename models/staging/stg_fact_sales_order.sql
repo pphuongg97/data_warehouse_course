7959ba24742ce9facc517f7429907db6f32482a8
pphuongg97 <phuongpham19.198@gmail.com>
2023-03-11 12:00:41 +0700

    change sales order key

--- models/staging/stg_fact_sales_order.sql
+++ models/staging/stg_fact_sales_order.sql
@@ -5,19 +5,19 @@ WITH stg_fact_sales_order__source AS(
 
 , stg_fact_sales_order__rename_column AS(
   SELECT
-  order_id AS order_key
+  order_id AS sales_order_key
   , customer_id AS customer_key
   FROM stg_fact_sales_order__source
 )
 
 , stg_fact_sales_order__cast_type AS(
   SELECT
-  CAST(order_key AS INT) AS order_key
+  CAST(sales_order_key AS INT) AS sales_order_key
   , CAST(customer_key AS INT) AS customer_key
 FROM stg_fact_sales_order__rename_column
 )
 
 SELECT
-order_key
+sales_order_key
 , customer_key
 FROM stg_fact_sales_order__cast_type
