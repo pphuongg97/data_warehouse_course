3bba8efae21fabe26ccb4296ac532ebc33124700
pphuongg97 <phuongpham19.198@gmail.com>
2023-03-16 19:17:54 +0700

    arrange  columnn

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -7,9 +7,10 @@ WITH dim_product__source as(
   SELECT 
   stock_item_id as product_key
   , stock_item_name as product_name
-  , brand as brand_name
-  , supplier_id as supplier_key
   , is_chiller_stock as is_chiller_stock
+  , supplier_id as supplier_key
+  , brand as brand_name
+    
   FROM dim_product__source
 )
 
@@ -17,9 +18,9 @@ WITH dim_product__source as(
   SELECT
   CAST (product_key as int) AS product_key
   , CAST (product_name as string) AS product_name
-  , CAST (brand_name as string) AS brand_name
-  , CAST (supplier_key as int) AS supplier_key
   , CAST (is_chiller_stock as boolean) AS is_chiller_stock
+  , CAST (supplier_key as int) AS supplier_key
+  , CAST (brand_name as string) AS brand_name
   FROM dim_product__rename_column
 )
 
@@ -27,24 +28,24 @@ WITH dim_product__source as(
   SELECT
   product_key
   , product_name
-  , brand_name
-  , supplier_key
   , CASE
       WHEN is_chiller_stock is TRUE THEN 'Chiller Stock'
       WHEN is_chiller_stock is FALSE THEN 'Not Chiller Stock'
       WHEN is_chiller_stock is NULL THEN 'Undefined'
       ELSE 'Unvalid' END 
     AS is_chiller_stock  
+  , supplier_key
+  , brand_name
   FROM dim_product__rename_column
 )
 
 SELECT
   dim_product.product_key
   , dim_product.product_name
-  , COALESCE(dim_product.brand_name,'Undefined') AS brand_name
+  , dim_product.is_chiller_stock
   , dim_product.supplier_key
   , COALESCE(dim_supplier.supplier_name,'Unvalied') AS supplier_name
-  , dim_product.is_chiller_stock
+  , COALESCE(dim_product.brand_name,'Undefined') AS brand_name  
   FROM dim_product__convert_boolean AS dim_product
   LEFT JOIN {{ ref('dim_supplier') }} AS dim_supplier
   ON dim_product.supplier_key = dim_supplier.supplier_key
