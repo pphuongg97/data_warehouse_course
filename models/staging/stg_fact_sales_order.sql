b48785dcf01f78502645437f6e6c99e78b440bd7
pphuongg97 <phuongpham19.198@gmail.com>
2023-03-16 19:18:59 +0700

    arrange column

--- models/staging/stg_fact_sales_order.sql
+++ models/staging/stg_fact_sales_order.sql
@@ -24,9 +24,9 @@ FROM stg_fact_sales_order__rename_column
 SELECT
 sales_order_key
 , fact_line.customer_key
-, fact_line.order_date
 , COALESCE(fact_line.picked_by_person_key,0) AS picked_by_person_key
 , fact_header.full_name AS full_name
+, fact_line.order_date
 FROM stg_fact_sales_order__cast_type AS fact_line
 LEFT JOIN {{ ref("dim_person")}} AS fact_header
 ON fact_line.picked_by_person_key = fact_header.person_key
