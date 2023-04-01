bf13b1a9c7e78a23f5237204de7fc838e95f1933
pphuongg97 <phuongpham19.198@gmail.com>
2023-04-01 15:40:47 +0700

    updated

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -5,11 +5,15 @@ WITH fact_sales_order_line__source AS (
 
 , fact_sales_order_line__rename_column AS(
 SELECT 
-  order_line_id as sales_order_line_key
-  , order_id as sales_order_key
-  , stock_item_id as product_key
-  , quantity as quantity
-  , unit_price as unit_price  
+  order_line_id AS sales_order_line_key
+  , order_id AS sales_order_key
+  , stock_item_id AS product_key
+  , quantity AS quantity
+  , unit_price AS unit_price
+  , tax_rate AS tax_rate
+  , picked_quantity AS picked_quantity
+  , package_type_id AS package_type_key
+  , picking_completed_when AS line_picking_completed_when
 FROM fact_sales_order_line__source
 )
 
@@ -19,19 +23,48 @@ FROM fact_sales_order_line__source
   , CAST(sales_order_key AS INT) AS sales_order_key
   , CAST(product_key AS INT) AS product_key
   , CAST(quantity AS INT) AS quantity
-  , CAST(unit_price AS numeric) AS unit_price
+  , CAST(unit_price AS NUMERIC) AS unit_price
+  , CAST(tax_rate AS NUMERIC) AS tax_rate
+  , CAST(picked_quantity AS INT) AS picked_quantity
+  , CAST(package_type_key AS INT) AS package_type_key
+  , CAST(line_picking_completed_when AS DATE) AS line_picking_completed_when
   FROM fact_sales_order_line__rename_column
+)
+
+, fact_sales_order_line__calculated_measure AS(
+  SELECT
+  *
+  , quantity * unit_price AS gross_amount
+  , tax_rate * quantity * unit_price AS tax_amount
+  , quantity * unit_price * (1 - tax_rate) AS net_amount
+  FROM fact_sales_order_line__cast_type
 )
 
 SELECT
 sales_order_line_key
 , fact_line.sales_order_key
-, fact_header.customer_key
+, fact_header.is_undersupply_backordered
 , fact_line.product_key
+, fact_line.quantity
+, fact_line.unit_price
+, fact_line.gross_amount
+, fact_line.tax_rate
+, fact_line.tax_amount
+, fact_line.picked_quantity
+, COALESCE(fact_header.customer_key,-1) AS customer_key
+, COALESCE(fact_header.salesperson_person_key,-1) AS salesperson_person_key
 , COALESCE(fact_header.picked_by_person_key,-1) AS picked_by_person_key
-, fact_header.full_name AS full_name
+, COALESCE(fact_header.contact_person_key,-1) AS contact_person_key
+, fact_line.package_type_key
 , fact_header.order_date
-, fact_line.quantity * fact_line.unit_price AS gross_amount
-FROM fact_sales_order_line__cast_type AS fact_line
+, fact_header.expected_delivery_date
+, fact_line.line_picking_completed_when
+, fact_header.picking_completed_when AS sales_order_picking_completed_when
+
+FROM fact_sales_order_line__calculated_measure AS fact_line
+
 LEFT JOIN {{ ref('stg_fact_sales_order') }} AS fact_header
-  ON fact_line.sales_order_key=fact_header.sales_order_key
+ON fact_line.sales_order_key=fact_header.sales_order_key
+
+
+
