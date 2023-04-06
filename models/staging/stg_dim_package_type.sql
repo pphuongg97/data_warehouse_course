96ffff401bae08c120e7304e43f0b7ba09f0f57f
pphuongg97 <phuongpham19.198@gmail.com>
2023-04-06 21:20:26 +0700

    add undefined record

--- models/staging/stg_dim_package_type.sql
+++ models/staging/stg_dim_package_type.sql
@@ -18,7 +18,24 @@ WITH stg_dim_package_type__source AS(
   FROM stg_dim_package_type__rename_column
 )
 
-SELECT
+, stg_dim_package_type__add_undefined_record AS(
+  SELECT
   package_type_key
   , package_type_name
   FROM stg_dim_package_type__cast_type
+
+  UNION ALL
+  SELECT
+  0 AS package_type_key
+  , 'Undefined' AS package_type_name
+
+  UNION ALL
+  SELECT
+  -1 AS package_type_key
+  , 'Unvalid' AS package_type_name
+)
+
+SELECT
+  package_type_key
+  , package_type_name
+  FROM stg_dim_package_type__add_undefined_record
