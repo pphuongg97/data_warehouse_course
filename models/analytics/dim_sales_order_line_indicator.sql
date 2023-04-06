41c8bc0137a1de6e20997796f2481f4c671aee4f
pphuongg97 <phuongpham19.198@gmail.com>
2023-04-06 20:20:31 +0700

    create key for model

--- models/analytics/dim_sales_order_line_indicator.sql
+++ models/analytics/dim_sales_order_line_indicator.sql
@@ -10,8 +10,12 @@ SELECT
 )
 
 SELECT
-  *
-FROM dim_is_under_supply_backed_order
+ CONCAT(dim_sales_order_line_indicator.is_under_supply_backed_order_boolean, "," , stg_dim_package_type.package_type_key) AS sales_order_line_indicator_key
+ , dim_sales_order_line_indicator.is_under_supply_backed_order_boolean
+ , dim_sales_order_line_indicator.is_under_supply_backed_order
+ , stg_dim_package_type.package_type_key
+ , stg_dim_package_type.package_type_name 
+FROM dim_sales_order_line_indicator
 
-CROSS JOIN stg_dim_package_type AS stg_dim_package_type
-ORDER BY 1, 3
+CROSS JOIN `learn-dbt-379601.wide_world_importers_dwh_staging.stg_dim_package_type` AS stg_dim_package_type
+
