63136173814fc8c557038c2428224685e4634a8e
pphuongg97 <phuongpham19.198@gmail.com>
2023-04-06 20:52:28 +0700

    convert dim sales order line indicator from string to int

--- models/analytics/dim_sales_order_line_indicator.sql
+++ models/analytics/dim_sales_order_line_indicator.sql
@@ -10,12 +10,12 @@ SELECT
 )
 
 SELECT
- CONCAT(dim_sales_order_line_indicator.is_under_supply_backed_order_boolean, "," , stg_dim_package_type.package_type_key) AS sales_order_line_indicator_key
+ FARM_FINGERPRINT( CONCAT(dim_sales_order_line_indicator.is_under_supply_backed_order_boolean, "," , stg_dim_package_type.package_type_key) ) AS sales_order_line_indicator_key
  , dim_sales_order_line_indicator.is_under_supply_backed_order_boolean
  , dim_sales_order_line_indicator.is_under_supply_backed_order
  , stg_dim_package_type.package_type_key
  , stg_dim_package_type.package_type_name 
 FROM dim_sales_order_line_indicator
 
-CROSS JOIN `learn-dbt-379601.wide_world_importers_dwh_staging.stg_dim_package_type` AS stg_dim_package_type
+CROSS JOIN {{ ref("stg_dim_package_type") }} AS stg_dim_package_type
 
