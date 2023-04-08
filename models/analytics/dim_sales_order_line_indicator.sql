ceb66da8665b4706dc7faaf045dfc7df653783c7
pphuongg97 <phuongpham19.198@gmail.com>
2023-04-08 16:18:54 +0700

    change name

--- models/analytics/dim_sales_order_line_indicator.sql
+++ models/analytics/dim_sales_order_line_indicator.sql
@@ -1,18 +1,18 @@
 WITH dim_sales_order_line_indicator AS(
 SELECT
-  TRUE AS is_under_supply_backed_order_boolean
-  , 'Under Supply Backed Order' AS is_under_supply_backed_order
+  TRUE AS is_undersupply_backedorder_boolean
+  , 'Under Supply Backed Order' AS is_undersupply_backedorder
 
 UNION ALL
 SELECT
-  FALSE AS is_under_supply_backed_order_boolean
-  , 'Not Under Supply Backed Order' AS is_under_supply_backed_order
+  FALSE AS is_undersupply_backedorder_boolean
+  , 'Not Under Supply Backed Order' AS is_undersupply_backedorder
 )
 
 SELECT
- FARM_FINGERPRINT( CONCAT(dim_sales_order_line_indicator.is_under_supply_backed_order, "," , stg_dim_package_type.package_type_key) ) AS sales_order_line_indicator_key
- , dim_sales_order_line_indicator.is_under_supply_backed_order_boolean
- , dim_sales_order_line_indicator.is_under_supply_backed_order
+ FARM_FINGERPRINT( CONCAT(dim_sales_order_line_indicator.is_undersupply_backedorder, "," , stg_dim_package_type.package_type_key) ) AS sales_order_line_indicator_key
+ , dim_sales_order_line_indicator.is_undersupply_backedorder_boolean
+ , dim_sales_order_line_indicator.is_undersupply_backedorder
  , stg_dim_package_type.package_type_key
  , stg_dim_package_type.package_type_name 
 FROM dim_sales_order_line_indicator
