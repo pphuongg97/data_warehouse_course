3710bdd288fb35741ca6bd29abcbb0af34ba3b04
pphuongg97 <phuongpham19.198@gmail.com>
2023-04-26 23:23:37 +0700

    b3 b4 b5

--- models/analytics/fact_salesperson_target.sql
+++ models/analytics/fact_salesperson_target.sql
@@ -16,3 +16,41 @@ WITH fact_salesperson_target__from_target AS(
     1,2
 )
 
+, fact_salesperson_target__join AS(
+SELECT
+  year_month
+  , salesperson_person_key
+  , COALESCE (fact_target.target_gross_amount, 0) AS target_gross_amount
+  , COALESCE (fact_actual.gross_amount, 0) AS gross_amount
+FROM fact_salesperson_target__from_target AS fact_target
+FULL OUTER JOIN fact_salesperson_target__from_sales AS fact_actual
+ USING (year_month, salesperson_person_key)
+)
+
+, fact_salesperson_target__calculate_achieve AS(
+SELECT
+  *
+  , gross_amount / IFNULL(target_gross_amount,0) AS achievement_percentage
+FROM fact_salesperson_target__join
+)
+
+, fact_salesperson_target__define_achievement AS(
+SELECT
+  *
+  , CASE
+      WHEN achievement_percentage < 0.95 THEN 'Not Achieved'
+      WHEN achievement_percentage >= 0.95 THEN 'Achieved'
+      WHEN achievement_percentage = 0 THEN 'No Target'
+      ELSE 'Invalid' END
+AS is_achieved
+FROM fact_salesperson_target__calculate_achieve
+)
+
+SELECT
+  year_month
+  , salesperson_person_key
+  , target_gross_amount
+  , gross_amount
+  , achievement_percentage
+  , is_achieved
+FROM fact_salesperson_target__define_achievement
